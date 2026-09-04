dfa87582bb8cb40f678575a9d9f47e130265ecc8
Jolivaresxz <jean.olivaresv@correoaiep.cl>
2026-09-04 11:07:23 -0400

    Creación de la interface ClienteRepository

---
+++ Clientes/IClienteRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+use Cliente;
+
+interface IClienteRepository
+{
+    public function guardar(Cliente $cliente): Cliente;
+    public function actualizar(Cliente $cliente): void;
+    public function obtenerPorId(int $id): ?Cliente;
+    public function obtenerPorRut(string $rut): ?Cliente;
+
+    /* Devuelve el objeto cliente */
+    public function obtenerTodos(): array;
+    public function eliminar(int $id): bool;
+}
